d1d536a8383099362259561e67ecd8ca4a46551e
Shiawase <73775954+Shiawaseu@users.noreply.github.com>
2024-08-13 20:05:33 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel'
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
   integrations: [react(), tailwind()],
   resolve: {
     alias: {
       '@': '/src'
     }
-  }
+  },
+  adapter: vercel()
 });
