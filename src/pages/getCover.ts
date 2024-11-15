b4323892f97baa1b583f85aac1100045b2b7bd69
Shiawase <73775954+Shiawaseu@users.noreply.github.com>
2024-11-15 23:37:29 +0300

    Update getCover.ts

--- src/pages/getCover.ts
+++ src/pages/getCover.ts
@@ -55,9 +55,7 @@ async function fetchSpotifyData(embedUrl: string, resolution: number) {
     }
 
     const pageJson = JSON.parse(pageProps);
-    // New update to the embed page
 
-    // For now resolutions are useless, but I'll keep the functionality if Spotify ever provides different resolutions.
     const entity = pageJson.props.pageProps.state.data.entity;
     const coverArt = entity.visualIdentity.image;
 
